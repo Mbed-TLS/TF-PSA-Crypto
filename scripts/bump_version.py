0e09bc7bec03ec35dadd8e04b92acba732178259
David Horstmann <david.horstmann@arm.com>
2023-09-19 15:50:31 +0100

    Revert copyright to Mbed TLS Contributors
    (Based on the copyright notice in scripts/psa_crypto.py)
    
    Signed-off-by: David Horstmann <david.horstmann@arm.com>

--- scripts/bump_version.py
+++ scripts/bump_version.py
@@ -5,7 +5,7 @@ This script sets the PSA Crypto version and SO version to the specified
 values.
 """
 
-# Copyright The PSA Crypto Contributors
+# Copyright The Mbed TLS Contributors
 # SPDX-License-Identifier: Apache-2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
