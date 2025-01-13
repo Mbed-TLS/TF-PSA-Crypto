a320a4bcc7825de84ca166f595dcff9d69912ed0
Harry Ramsey <harry.ramsey@arm.com>
2025-01-13 10:51:08 +0000

    Add check python files component
    This commit adds the check python files component to TF-PSA-Crypto.
    
    Signed-off-by: Harry Ramsey <harry.ramsey@arm.com>

--- tests/scripts/components-basic-checks.sh
+++ tests/scripts/components-basic-checks.sh
@@ -13,3 +13,8 @@ component_tf_psa_crypto_check_files () {
     msg "Check: file sanity checks (permissions, encodings)" # < 1s
     $FRAMEWORK/scripts/check_files.py
 }
+
+components_tf_psa_crypto_check_python_files () {
+    msg "Lint: Python scripts"
+    $FRAMEWORK/scripts/check-python-files.sh
+}
