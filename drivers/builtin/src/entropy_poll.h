141f639c0a6b8f80d4525cab3424cf604ad5f8af
Valerio Setti <vsetti@baylibre.com>
2025-03-18 14:27:59 +0100

    drivers: add wrapper for mbedtls_platform_get_entropy_alt()
    This allows the newly created function to be used in the entropy module
    alogside other entropy polling functions.
    
    Signed-off-by: Valerio Setti <vsetti@baylibre.com>

--- drivers/builtin/src/entropy_poll.h
+++ drivers/builtin/src/entropy_poll.h
@@ -41,6 +41,35 @@ int mbedtls_nv_seed_poll(void *data,
                          unsigned char *output, size_t len, size_t *olen);
 #endif
 
+/* Wrapper to allow mbedtls_platform_get_entropy_alt() to be used from entropy
+ * module as other entropy polling functions (i.e. mbedtls_platform_entropy_poll
+ * and mbedtls_nv_seed_poll). */
+#if defined(MBEDTLS_PLATFORM_GET_ENTROPY_ALT)
+
+#include <mbedtls/platform.h>
+
+#define MBEDTLS_ENTROPY_MIN_HARDWARE            32
+
+static inline int mbedtls_hardware_poll(void *data, unsigned char *output,
+                                        size_t len, size_t *olen)
+{
+    int ret;
+    size_t entropy_content = 0;
+    (void) data;
+
+    ret = mbedtls_platform_get_entropy_alt(output, len, olen, &entropy_content);
+    if (ret != 0) {
+        return ret;
+    }
+
+    if (entropy_content < (8 * (*olen))) {
+        return MBEDTLS_ERR_ENTROPY_SOURCE_FAILED;
+    }
+
+    return 0;
+}
+#endif /* MBEDTLS_PLATFORM_GET_ENTROPY_ALT */
+
 #ifdef __cplusplus
 }
 #endif
