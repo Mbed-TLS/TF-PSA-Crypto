c0ea1e3880f83c33843019c1dc95eab139bf55ef
Ben Taylor <ben.taylor@linaro.org>
2025-05-09 08:12:16 +0100

    optimise key type checking in NIST KW and ensure psa_reset_attributes is called in all error cases
    Signed-off-by: Ben Taylor <ben.taylor@linaro.org>

--- drivers/builtin/src/nist_kw.c
+++ drivers/builtin/src/nist_kw.c
@@ -57,17 +57,18 @@ static int verify_input(mbedtls_svc_key_id_t *key){
     psa_key_attributes_t attributes;
     ret = psa_get_key_attributes(*key, &attributes);
 
-    if (ret != PSA_SUCCESS) {
-        return ret;
+    if (ret == PSA_SUCCESS) {
+
+       /*
+        * Currently NIST KW only supports PSA_KEY_TYPE_AES, so verify this is
+        * set in the key attributes.
+        */
+
+        if(psa_get_key_type(&attributes) != PSA_KEY_TYPE_AES) {
+            ret = PSA_ERROR_INVALID_ARGUMENT;
+        }
     }
 
-    /*
-     * Currently NIST KW only supports PSA_KEY_TYPE_AES, so verify this is
-     * set in the key attributes.
-     */
-    if (psa_get_key_type(&attributes) != PSA_KEY_TYPE_AES) {
-        return PSA_ERROR_INVALID_ARGUMENT;
-    }
     psa_reset_key_attributes(&attributes);
 
     return ret;
