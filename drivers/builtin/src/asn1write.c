1a2624134a98e79c9ba6d5b4fe083d77e91d31b2
David Horstmann <david.horstmann@arm.com>
2025-06-26 14:07:58 +0100

    Do not attempt to memset() a zero-length buffer
    It is possible that the pointer to the buffer is NULL.
    
    Signed-off-by: David Horstmann <david.horstmann@arm.com>

--- drivers/builtin/src/asn1write.c
+++ drivers/builtin/src/asn1write.c
@@ -466,7 +466,9 @@ int mbedtls_asn1_write_integer(unsigned char **p,
         return MBEDTLS_ERR_ASN1_BUF_TOO_SMALL;//TC3 buffer less than integer size.
     }
 
-    memset(start, 0, output_buffer_size);
+    if (start != *p) {
+        memset(start, 0, output_buffer_size);
+    }
 
     /* Special case - if integer_length is zero, the value is zero and it
      * should be encoded as one byte. */
