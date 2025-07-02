300cd707cd37eeb1df01b38e6787d6cbbf58ad7b
Ben Taylor <ben.taylor@linaro.org>
2025-07-02 14:44:54 +0100

    rename header to pk_private.h
    Signed-off-by: Ben Taylor <ben.taylor@linaro.org>

--- include/mbedtls/private/pk.h
+++
@@ -1,6 +0,0 @@
-#ifndef MBEDTLS_PK_PRIVATE_H
-#define MBEDTLS_PK_PRIVATE_H
-#include <mbedtls/pk.h>
-#if defined(MBEDTLS_DECLARE_PRIVATE_IDENTIFIERS)
-#endif
-#endif /* MBEDTLS_PK_PRIVATE_H */
---
+++ include/mbedtls/private/pk_private.h
@@ -0,0 +1,6 @@
+#ifndef MBEDTLS_PRIVATE_PK_H
+#define MBEDTLS_PRIVATE_PK_H
+#include <mbedtls/pk.h>
+#if defined(MBEDTLS_DECLARE_PRIVATE_IDENTIFIERS)
+#endif
+#endif /* MBEDTLS_PRIVATE_PK_H */
