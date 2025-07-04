71c4279e78e79dbd3aa8236c4e5e17c1d0c2444c
Ben Taylor <ben.taylor@linaro.org>
2025-07-04 08:36:15 +0100

    Add comment boilerplate and copyright
    Signed-off-by: Ben Taylor <ben.taylor@linaro.org>

--- include/mbedtls/private/pk_private.h
+++ include/mbedtls/private/pk_private.h
@@ -1,3 +1,13 @@
+/**
+ * \file pk_private.h
+ *
+ * \brief Private Public Key abstraction layer
+ */
+/*
+ *  Copyright The Mbed TLS Contributors
+ *  SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later
+ */
+
 #ifndef MBEDTLS_PRIVATE_PK_PRIVATE_H
 #define MBEDTLS_PRIVATE_PK_PRIVATE_H
 #include <mbedtls/pk.h>
