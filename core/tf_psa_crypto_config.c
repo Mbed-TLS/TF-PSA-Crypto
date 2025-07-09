aad9e448e8b47b349c9ed6832cac6432ef6b4f6e
Gilles Peskine <Gilles.Peskine@arm.com>
2025-07-09 16:16:45 +0200

    New source file for configuration checks
    This will be populated in subsequent commits.
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

---
+++ core/tf_psa_crypto_config.c
@@ -0,0 +1,9 @@
+/*
+ *  TF-PSA-Crypto configuration checks
+ */
+/*
+ *  Copyright The Mbed TLS Contributors
+ *  SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later
+ */
+
+#include <tf-psa-crypto/build_info.h>
