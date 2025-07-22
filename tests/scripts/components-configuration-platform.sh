c23c5db374b63c3c17c6913e8900cd52bf704f73
Gilles Peskine <Gilles.Peskine@arm.com>
2025-07-22 21:11:44 +0200

    Fix seedfile location
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- tests/scripts/components-configuration-platform.sh
+++ tests/scripts/components-configuration-platform.sh
@@ -53,7 +53,7 @@ component_test_entropy_nv_seed_only () {
 
     # Check that the library seems to refer to the seedfile, but not to
     # platform entropy sources.
-    grep seedfile library/platform.o
+    grep seedfile drivers/builtin/CMakeFiles/builtin_objs.dir/src/platform.c.o
     not grep getrandom drivers/builtin/CMakeFiles/builtin_objs.dir/src/entropy*.o drivers/builtin/CMakeFiles/builtin_objs.dir/src/platform*.o
     not grep /dev/random drivers/builtin/CMakeFiles/builtin_objs.dir/src/entropy*.o drivers/builtin/CMakeFiles/builtin_objs.dir/src/platform*.o
     not grep /dev/.random drivers/builtin/CMakeFiles/builtin_objs.dir/src/entropy*.o drivers/builtin/CMakeFiles/builtin_objs.dir/src/platform*.o
