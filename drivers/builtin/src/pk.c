543f3f216769d892f2c9182154d5ae138a336352
Ben Taylor <ben.taylor@linaro.org>
2025-07-31 11:09:42 +0100

    Add in additional type fixes
    Signed-off-by: Ben Taylor <ben.taylor@linaro.org>

--- drivers/builtin/src/pk.c
+++ drivers/builtin/src/pk.c
@@ -1051,7 +1051,7 @@ int mbedtls_pk_verify_new(mbedtls_pk_type_t type, mbedtls_pk_context *ctx,
                           mbedtls_md_type_t md_alg, const unsigned char *hash,
                           size_t hash_len, const unsigned char *sig, size_t sig_len)
 {
-    return mbedtls_pk_verify_ext(type, ctx, md_alg, hash, hash_len, sig, sig_len);
+    return mbedtls_pk_verify_ext((mbedtls_pk_sigalg_t) type, ctx, md_alg, hash, hash_len, sig, sig_len);
 }
 
 /*
