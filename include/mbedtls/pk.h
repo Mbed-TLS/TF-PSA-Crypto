2f6edb6a542ac54da644eb39d9474d8e3ae5d9e3
Ben Taylor <ben.taylor@linaro.org>
2025-08-05 14:20:51 +0100

    Fix rebase removal of mbedtls_pk_sigalg_t
    Signed-off-by: Ben Taylor <ben.taylor@linaro.org>

--- include/mbedtls/pk.h
+++ include/mbedtls/pk.h
@@ -67,6 +67,13 @@
 extern "C" {
 #endif
 
+typedef enum {
+    MBEDTLS_PK_SIGALG_NONE = 0,
+    MBEDTLS_PK_SIGALG_RSA_PKCS1V15,
+    MBEDTLS_PK_SIGALG_RSA_PSS,
+    MBEDTLS_PK_SIGALG_ECDSA,
+} mbedtls_pk_sigalg_t;
+
 /**
  * \brief           Maximum size of a signature made by mbedtls_pk_sign().
  */
