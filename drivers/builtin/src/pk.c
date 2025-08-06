d0ba7f7ed0cb92ad7a3988d254d34cce80d964a1
Ben Taylor <ben.taylor@linaro.org>
2025-08-06 13:57:37 +0100

    Revert previous commit
    Signed-off-by: Ben Taylor <ben.taylor@linaro.org>

--- drivers/builtin/src/pk.c
+++ drivers/builtin/src/pk.c
@@ -1047,11 +1047,11 @@ int mbedtls_pk_verify_ext(mbedtls_pk_sigalg_t type,
 /*
  * Verify a signature
  */
-int mbedtls_pk_verify_new(mbedtls_pk_sigalg_t type, mbedtls_pk_context *ctx,
+int mbedtls_pk_verify_new(mbedtls_pk_type_t type, mbedtls_pk_context *ctx,
                           mbedtls_md_type_t md_alg, const unsigned char *hash,
                           size_t hash_len, const unsigned char *sig, size_t sig_len)
 {
-    return mbedtls_pk_verify_ext(type,
+    return mbedtls_pk_verify_ext((mbedtls_pk_sigalg_t) type,
                                  ctx,
                                  md_alg,
                                  hash,
