dc98da9165ca401849956503c48dffa85e9acade
Ronald Cron <ronald.cron@arm.com>
2025-09-08 15:26:23 +0200

    Update crypto_config_profile_medium.h PSA API section
    Signed-off-by: Ronald Cron <ronald.cron@arm.com>

--- configs/ext/crypto_config_profile_medium.h
+++ configs/ext/crypto_config_profile_medium.h
@@ -26,6 +26,7 @@
 //#define PSA_WANT_ALG_CBC_NO_PADDING             1
 //#define PSA_WANT_ALG_CBC_PKCS7                  1
 #define PSA_WANT_ALG_CCM                        1
+//#define PSA_WANT_ALG_CCM_STAR_NO_TAG            1
 //#define PSA_WANT_ALG_CMAC                       1
 //#define PSA_WANT_ALG_CFB                        1
 //#define PSA_WANT_ALG_CHACHA20_POLY1305          1
@@ -33,15 +34,18 @@
 //#define PSA_WANT_ALG_DETERMINISTIC_ECDSA        1
 //#define PSA_WANT_ALG_ECB_NO_PADDING             1
 #define PSA_WANT_ALG_ECDH                       1
+//#define PSA_WANT_ALG_FFDH                       1
 #define PSA_WANT_ALG_ECDSA                      1
+//#define PSA_WANT_ALG_JPAKE                      1
 //#define PSA_WANT_ALG_GCM                        1
 #define PSA_WANT_ALG_HKDF                       1
+//#define PSA_WANT_ALG_HKDF_EXTRACT               1
+//#define PSA_WANT_ALG_HKDF_EXPAND                1
 #define PSA_WANT_ALG_HMAC                       1
 //#define PSA_WANT_ALG_MD5                        1
 //#define PSA_WANT_ALG_OFB                        1
-/* PBKDF2-HMAC is not yet supported via the PSA API in Mbed TLS.
- * Note: when adding support, also adjust include/mbedtls/config_psa.h */
 //#define PSA_WANT_ALG_PBKDF2_HMAC                1
+//#define PSA_WANT_ALG_PBKDF2_AES_CMAC_PRF_128    1
 //#define PSA_WANT_ALG_RIPEMD160                  1
 //#define PSA_WANT_ALG_RSA_OAEP                   1
 //#define PSA_WANT_ALG_RSA_PKCS1V15_CRYPT         1
@@ -52,36 +56,50 @@
 #define PSA_WANT_ALG_SHA_256                    1
 //#define PSA_WANT_ALG_SHA_384                    1
 //#define PSA_WANT_ALG_SHA_512                    1
+//#define PSA_WANT_ALG_SHA3_224                   1
+//#define PSA_WANT_ALG_SHA3_256                   1
+//#define PSA_WANT_ALG_SHA3_384                   1
+//#define PSA_WANT_ALG_SHA3_512                   1
 //#define PSA_WANT_ALG_STREAM_CIPHER              1
 #define PSA_WANT_ALG_TLS12_PRF                  1
 #define PSA_WANT_ALG_TLS12_PSK_TO_MS            1
-/* PBKDF2-HMAC is not yet supported via the PSA API in Mbed TLS.
- * Note: when adding support, also adjust include/mbedtls/config_psa.h */
-//#define PSA_WANT_ALG_XTS                        1
+//#define PSA_WANT_ALG_TLS12_ECJPAKE_TO_PMS       1
 
 //#define PSA_WANT_ECC_BRAINPOOL_P_R1_256         1
 //#define PSA_WANT_ECC_BRAINPOOL_P_R1_384         1
 //#define PSA_WANT_ECC_BRAINPOOL_P_R1_512         1
 //#define PSA_WANT_ECC_MONTGOMERY_255             1
 //#define PSA_WANT_ECC_MONTGOMERY_448             1
-//#define PSA_WANT_ECC_SECP_K1_192                1
 //#define PSA_WANT_ECC_SECP_K1_256                1
-//#define PSA_WANT_ECC_SECP_R1_192                1
+/* For secp256r1, consider enabling #MBEDTLS_PSA_P256M_DRIVER_ENABLED
+ * (see the description in psa/cypto_config.h for details). */
 #define PSA_WANT_ECC_SECP_R1_256                1
 //#define PSA_WANT_ECC_SECP_R1_384                1
 //#define PSA_WANT_ECC_SECP_R1_521                1
+/* These 2 curves are not part of the public API. They are kept for internal
+ * testing only, but they might be removed in a future version of the
+ * library. */
+//#define PSA_WANT_ECC_SECP_K1_192                1
+//#define PSA_WANT_ECC_SECP_R1_192                1
+
+//#define PSA_WANT_DH_RFC7919_2048                1
+//#define PSA_WANT_DH_RFC7919_3072                1
+//#define PSA_WANT_DH_RFC7919_4096                1
+//#define PSA_WANT_DH_RFC7919_6144                1
+//#define PSA_WANT_DH_RFC7919_8192                1
 
 #define PSA_WANT_KEY_TYPE_DERIVE                1
+//#define PSA_WANT_KEY_TYPE_PASSWORD              1
+//#define PSA_WANT_KEY_TYPE_PASSWORD_HASH         1
 #define PSA_WANT_KEY_TYPE_HMAC                  1
 #define PSA_WANT_KEY_TYPE_AES                   1
 //#define PSA_WANT_KEY_TYPE_ARIA                  1
 //#define PSA_WANT_KEY_TYPE_CAMELLIA              1
 //#define PSA_WANT_KEY_TYPE_CHACHA20              1
 //#define PSA_WANT_KEY_TYPE_DES                   1
-//#define PSA_WANT_KEY_TYPE_ECC_KEY_PAIR          1 /* Deprecated */
 #define PSA_WANT_KEY_TYPE_ECC_PUBLIC_KEY        1
+//#define PSA_WANT_KEY_TYPE_DH_PUBLIC_KEY         1
 #define PSA_WANT_KEY_TYPE_RAW_DATA              1
-//#define PSA_WANT_KEY_TYPE_RSA_KEY_PAIR          1 /* Deprecated */
 //#define PSA_WANT_KEY_TYPE_RSA_PUBLIC_KEY        1
 
 /*
@@ -97,6 +115,17 @@
 #define PSA_WANT_KEY_TYPE_ECC_KEY_PAIR_GENERATE 1
 //#define PSA_WANT_KEY_TYPE_ECC_KEY_PAIR_DERIVE   1
 
+//#define PSA_WANT_KEY_TYPE_RSA_KEY_PAIR_BASIC      1
+//#define PSA_WANT_KEY_TYPE_RSA_KEY_PAIR_IMPORT   1
+//#define PSA_WANT_KEY_TYPE_RSA_KEY_PAIR_EXPORT   1
+//#define PSA_WANT_KEY_TYPE_RSA_KEY_PAIR_GENERATE 1
+//#define PSA_WANT_KEY_TYPE_RSA_KEY_PAIR_DERIVE   1 /* Not supported */
+
+//#define PSA_WANT_KEY_TYPE_DH_KEY_PAIR_BASIC       1
+//#define PSA_WANT_KEY_TYPE_DH_KEY_PAIR_IMPORT    1
+//#define PSA_WANT_KEY_TYPE_DH_KEY_PAIR_EXPORT    1
+//#define PSA_WANT_KEY_TYPE_DH_KEY_PAIR_GENERATE  1
+//#define PSA_WANT_KEY_TYPE_DH_KEY_PAIR_DERIVE    1 /* Not supported */
 /** \} name SECTION Cryptographic mechanism selection (PSA API) */
 
 /**
