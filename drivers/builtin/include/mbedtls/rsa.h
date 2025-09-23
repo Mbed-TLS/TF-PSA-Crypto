5622d176c40266cad11394b16a59c139883d8d49
Gilles Peskine <Gilles.Peskine@arm.com>
2025-09-23 19:04:09 +0200

    Add shadow versions of removed headers
    Resolves #473.
    
    ```
    for h in $headers; do printf '%s\n' '/*' ' *  Copyright The Mbed TLS Contributors' ' *  SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later' ' */' '' '#error "<mbedtls/'$h'> no longer exists in TF-PSA-Crypto 1.x or Mbed TLS 4.x"' >drivers/builtin/include/mbedtls/$h; done
    ```
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

---
+++ drivers/builtin/include/mbedtls/rsa.h
@@ -0,0 +1,6 @@
+/*
+ *  Copyright The Mbed TLS Contributors
+ *  SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later
+ */
+
+#error "<mbedtls/rsa.h> no longer exists in TF-PSA-Crypto 1.x or Mbed TLS 4.x"
