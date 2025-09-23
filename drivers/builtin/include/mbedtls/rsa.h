4693abf0afc84bfa99e7eb4c9b3a9a981890f205
Gilles Peskine <Gilles.Peskine@arm.com>
2025-09-23 19:38:07 +0200

    Don't error out when building query_config.c
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- drivers/builtin/include/mbedtls/rsa.h
+++ drivers/builtin/include/mbedtls/rsa.h
@@ -3,4 +3,6 @@
  *  SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later
  */
 
+#if !defined(MBEDTLS_PROGRAMS_TEST_QUERY_CONFIG_H)
 #error "<mbedtls/rsa.h> no longer exists in TF-PSA-Crypto 1.x or Mbed TLS 4.x"
+#endif
