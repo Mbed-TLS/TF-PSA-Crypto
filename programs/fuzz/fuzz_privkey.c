2064fe5043deef79f2bbd12db8bf3a76ff96a1e0
Valerio Setti <vsetti@baylibre.com>
2025-10-03 22:34:12 +0200

    programs: fuzz: update fuzz_[priv|pub]key
    Update both executables as follows:
    - remove mbedtls_pk_[rsa|ec] replacing them with a key export and a check
      against the key that was previously parsed;
    - remove legacy constructs (ex: entropy, ctr-drbg, ...) which are no more
      needed.
    
    Signed-off-by: Valerio Setti <vsetti@baylibre.com>

--- programs/fuzz/fuzz_privkey.c
+++ programs/fuzz/fuzz_privkey.c
@@ -2,102 +2,64 @@
 
 #include <stdint.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include "mbedtls/pk.h"
-#include "mbedtls/private/pk_private.h"
-#include "mbedtls/private/entropy.h"
-#include "mbedtls/private/ctr_drbg.h"
-#include "mbedtls/private/pk_private.h"
 #include "fuzz_common.h"
 
 //4 Kb should be enough for every bug ;-)
 #define MAX_LEN 0x1000
 
-#if defined(MBEDTLS_PK_PARSE_C) && defined(MBEDTLS_CTR_DRBG_C) && defined(MBEDTLS_ENTROPY_C)
-const char *pers = "fuzz_privkey";
-#endif // MBEDTLS_PK_PARSE_C && MBEDTLS_CTR_DRBG_C && MBEDTLS_ENTROPY_C
-
 int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
 {
-#if defined(MBEDTLS_PK_PARSE_C) && defined(MBEDTLS_CTR_DRBG_C) && defined(MBEDTLS_ENTROPY_C)
+#if defined(MBEDTLS_PK_PARSE_C) && defined(MBEDTLS_PK_WRITE_C)
     int ret;
     mbedtls_pk_context pk;
-    mbedtls_ctr_drbg_context ctr_drbg;
-    mbedtls_entropy_context entropy;
+    uint8_t *out_buf = NULL;
 
     if (Size > MAX_LEN) {
         //only work on small inputs
         Size = MAX_LEN;
     }
 
-    mbedtls_ctr_drbg_init(&ctr_drbg);
-    mbedtls_entropy_init(&entropy);
     mbedtls_pk_init(&pk);
 
     psa_status_t status = psa_crypto_init();
     if (status != PSA_SUCCESS) {
-        goto exit;
-    }
-
-    if (mbedtls_ctr_drbg_seed(&ctr_drbg, dummy_entropy, &entropy,
-                              (const unsigned char *) pers, strlen(pers)) != 0) {
-        goto exit;
+        fprintf(stderr, "Error: failed to initialize PSA\n");
+        abort();
     }
 
     ret = mbedtls_pk_parse_key(&pk, Data, Size, NULL, 0);
-    if (ret == 0) {
-#if defined(MBEDTLS_RSA_C)
-        if (mbedtls_pk_get_type(&pk) == MBEDTLS_PK_RSA) {
-            mbedtls_mpi N, P, Q, D, E, DP, DQ, QP;
-            mbedtls_rsa_context *rsa;
-
-            mbedtls_mpi_init(&N); mbedtls_mpi_init(&P); mbedtls_mpi_init(&Q);
-            mbedtls_mpi_init(&D); mbedtls_mpi_init(&E); mbedtls_mpi_init(&DP);
-            mbedtls_mpi_init(&DQ); mbedtls_mpi_init(&QP);
-
-            rsa = mbedtls_pk_rsa(pk);
-            if (mbedtls_rsa_export(rsa, &N, &P, &Q, &D, &E) != 0) {
-                abort();
-            }
-            if (mbedtls_rsa_export_crt(rsa, &DP, &DQ, &QP) != 0) {
-                abort();
-            }
-
-            mbedtls_mpi_free(&N); mbedtls_mpi_free(&P); mbedtls_mpi_free(&Q);
-            mbedtls_mpi_free(&D); mbedtls_mpi_free(&E); mbedtls_mpi_free(&DP);
-            mbedtls_mpi_free(&DQ); mbedtls_mpi_free(&QP);
-        } else
-#endif
-#if defined(MBEDTLS_ECP_C)
-        if (mbedtls_pk_get_type(&pk) == MBEDTLS_PK_ECKEY ||
-            mbedtls_pk_get_type(&pk) == MBEDTLS_PK_ECKEY_DH) {
-            mbedtls_ecp_keypair *ecp = mbedtls_pk_ec(pk);
-            mbedtls_ecp_group_id grp_id = mbedtls_ecp_keypair_get_group_id(ecp);
-            const mbedtls_ecp_curve_info *curve_info =
-                mbedtls_ecp_curve_info_from_grp_id(grp_id);
-
-            /* If the curve is not supported, the key should not have been
-             * accepted. */
-            if (curve_info == NULL) {
-                abort();
-            }
-        } else
-#endif
-        {
-            /* The key is valid but is not of a supported type.
-             * This should not happen. */
-            abort();
-        }
+    if (ret != 0) {
+        fprintf(stderr, "Error: key parsing failed\n");
+        abort();
     }
-exit:
-    mbedtls_entropy_free(&entropy);
-    mbedtls_ctr_drbg_free(&ctr_drbg);
+
+    out_buf = malloc(Size);
+
+    ret = mbedtls_pk_write_key_der(&pk, out_buf, Size);
+    if (ret <= 0) {
+        fprintf(stderr, "Error: key writing failed\n");
+        abort();
+    }
+
+    if (memcmp(Data, out_buf, Size) != 0) {
+        fprintf(stderr, "Error: exported key doesn´t match\n");
+        abort();
+    }
+
+    fprintf(stderr, "OK\n");
+
+    if (out_buf != NULL) {
+        free(out_buf);
+    }
     mbedtls_pk_free(&pk);
     mbedtls_psa_crypto_free();
-#else // MBEDTLS_PK_PARSE_C && MBEDTLS_CTR_DRBG_C && MBEDTLS_ENTROPY_C
+#else /* MBEDTLS_PK_PARSE_C && MBEDTLS_PK_WRITE_C */
     (void) Data;
     (void) Size;
-#endif // MBEDTLS_PK_PARSE_C && MBEDTLS_CTR_DRBG_C && MBEDTLS_ENTROPY_C
+#endif /* MBEDTLS_PK_PARSE_C && MBEDTLS_PK_WRITE_C */
 
     return 0;
 }
--- programs/fuzz/fuzz_pubkey.c
+++ programs/fuzz/fuzz_pubkey.c
@@ -2,89 +2,55 @@
 
 #include <stdint.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include "mbedtls/pk.h"
 #include "fuzz_common.h"
-#include "mbedtls/private/pk_private.h"
 
 int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
 {
-#ifdef MBEDTLS_PK_PARSE_C
+#if defined(MBEDTLS_PK_PARSE_C) && defined(MBEDTLS_PK_WRITE_C)
     int ret;
     mbedtls_pk_context pk;
+    uint8_t *out_buf = NULL;
 
     mbedtls_pk_init(&pk);
     psa_status_t status = psa_crypto_init();
     if (status != PSA_SUCCESS) {
-        goto exit;
+        fprintf(stderr, "Error: failed to initialize PSA\n");
+        abort();
     }
+
     ret = mbedtls_pk_parse_public_key(&pk, Data, Size);
-    if (ret == 0) {
-#if defined(MBEDTLS_RSA_C)
-        if (mbedtls_pk_get_type(&pk) == MBEDTLS_PK_RSA) {
-            mbedtls_mpi N, P, Q, D, E, DP, DQ, QP;
-            mbedtls_rsa_context *rsa;
-
-            mbedtls_mpi_init(&N); mbedtls_mpi_init(&P); mbedtls_mpi_init(&Q);
-            mbedtls_mpi_init(&D); mbedtls_mpi_init(&E); mbedtls_mpi_init(&DP);
-            mbedtls_mpi_init(&DQ); mbedtls_mpi_init(&QP);
-
-            rsa = mbedtls_pk_rsa(pk);
-            if (mbedtls_rsa_export(rsa, &N, NULL, NULL, NULL, &E) != 0) {
-                abort();
-            }
-            if (mbedtls_rsa_export(rsa, &N, &P, &Q, &D, &E) != MBEDTLS_ERR_RSA_BAD_INPUT_DATA) {
-                abort();
-            }
-            if (mbedtls_rsa_export_crt(rsa, &DP, &DQ, &QP) != MBEDTLS_ERR_RSA_BAD_INPUT_DATA) {
-                abort();
-            }
-
-            mbedtls_mpi_free(&N); mbedtls_mpi_free(&P); mbedtls_mpi_free(&Q);
-            mbedtls_mpi_free(&D); mbedtls_mpi_free(&E); mbedtls_mpi_free(&DP);
-            mbedtls_mpi_free(&DQ); mbedtls_mpi_free(&QP);
-
-        } else
-#endif
-#if defined(MBEDTLS_ECP_C)
-        if (mbedtls_pk_get_type(&pk) == MBEDTLS_PK_ECKEY ||
-            mbedtls_pk_get_type(&pk) == MBEDTLS_PK_ECKEY_DH) {
-            mbedtls_ecp_keypair *ecp = mbedtls_pk_ec(pk);
-            mbedtls_ecp_group_id grp_id = mbedtls_ecp_keypair_get_group_id(ecp);
-            const mbedtls_ecp_curve_info *curve_info =
-                mbedtls_ecp_curve_info_from_grp_id(grp_id);
-
-            /* If the curve is not supported, the key should not have been
-             * accepted. */
-            if (curve_info == NULL) {
-                abort();
-            }
-
-            /* It's a public key, so the private value should not have
-             * been changed from its initialization to 0. */
-            mbedtls_mpi d;
-            mbedtls_mpi_init(&d);
-            if (mbedtls_ecp_export(ecp, NULL, &d, NULL) != 0) {
-                abort();
-            }
-            if (mbedtls_mpi_cmp_int(&d, 0) != 0) {
-                abort();
-            }
-            mbedtls_mpi_free(&d);
-        } else
-#endif
-        {
-            /* The key is valid but is not of a supported type.
-             * This should not happen. */
-            abort();
-        }
+    if (ret != 0) {
+        fprintf(stderr, "Error: key parsing failed\n");
+        abort();
     }
-exit:
+
+    out_buf = malloc(Size);
+
+    ret = mbedtls_pk_write_pubkey_der(&pk, out_buf, Size);
+    if (ret <= 0) {
+        fprintf(stderr, "Error: key writing failed\n");
+        abort();
+    }
+
+    if (memcmp(Data, out_buf, Size) != 0) {
+        fprintf(stderr, "Error: exported key doesn´t match\n");
+        abort();
+    }
+
+    fprintf(stderr, "OK\n");
+
+    if (out_buf != NULL) {
+        free(out_buf);
+    }
     mbedtls_psa_crypto_free();
     mbedtls_pk_free(&pk);
-#else
+#else /* MBEDTLS_PK_PARSE_C && MBEDTLS_PK_WRITE_C */
     (void) Data;
     (void) Size;
-#endif //MBEDTLS_PK_PARSE_C
+#endif /* MBEDTLS_PK_PARSE_C && MBEDTLS_PK_WRITE_C */
 
     return 0;
 }
