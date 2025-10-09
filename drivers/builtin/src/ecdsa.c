b4d0bdf63204b18863d609df8151c4795803d665
Ben Taylor <ben.taylor@linaro.org>
2025-10-09 10:24:05 +0100

    Correct doxygen docs for mbedtls_ecdsa_write_signature and add catch for cases where f_rng is NULL
    Signed-off-by: Ben Taylor <ben.taylor@linaro.org>

--- drivers/builtin/src/ecdsa.c
+++ drivers/builtin/src/ecdsa.c
@@ -670,6 +670,9 @@ int mbedtls_ecdsa_write_signature(mbedtls_ecdsa_context *ctx,
                                   int (*f_rng)(void *, unsigned char *, size_t),
                                   void *p_rng)
 {
+    if(f_rng == NULL) {
+        return PSA_ERROR_INVALID_ARGUMENT;
+    }
     return mbedtls_ecdsa_write_signature_restartable(
         ctx, md_alg, hash, hlen, sig, sig_size, slen,
         f_rng, p_rng, NULL);
