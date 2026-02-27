ab57f6bd984ea99891d90f0a355008bf8c91d1a0
Dave Rodgman <dave.rodgman@arm.com>
2026-02-27 17:20:33 +0000

    clang size and perf: pmull_low
    Signed-off-by: Dave Rodgman <dave.rodgman@arm.com>

--- drivers/builtin/src/aesce.c
+++ drivers/builtin/src/aesce.c
@@ -611,8 +611,8 @@ static inline uint8x16_t pmull_low(uint8x16_t a, uint8x16_t b)
 
     return vreinterpretq_u8_p128(
         MBEDTLS_VMULL_P64(
-            (poly64_t) vget_low_p64(vreinterpretq_p64_u8(a)),
-            (poly64_t) vget_low_p64(vreinterpretq_p64_u8(b))
+            (poly64_t) vreinterpret_p64_u8(vget_low_u8(a)),
+            (poly64_t) vreinterpret_p64_u8(vget_low_u8(b))
             ));
 }
 
