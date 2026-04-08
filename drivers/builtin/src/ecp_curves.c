aa3a6811a64a3c27bbdddc550693e502ea58bcb9
Manuel Pégourié-Gonnard <manuel.pegourie-gonnard@arm.com>
2026-04-08 12:34:27 +0200

    ecp: use ecp_curves_new always
    Signed-off-by: Manuel Pégourié-Gonnard <manuel.pegourie-gonnard@arm.com>

--- drivers/builtin/src/ecp_curves.c
+++ drivers/builtin/src/ecp_curves.c
@@ -7,7 +7,7 @@
 
 #include "tf_psa_crypto_common.h"
 
-#if !defined(MBEDTLS_ECP_WITH_MPI_UINT)
+#if 0 && !defined(MBEDTLS_ECP_WITH_MPI_UINT)
 
 #if defined(MBEDTLS_ECP_LIGHT)
 
@@ -4342,4 +4342,4 @@ static int ecp_mod_p256k1(mbedtls_mpi *N)
 #endif /* MBEDTLS_ECP_DP_SECP256K1_ENABLED */
 
 #endif /* MBEDTLS_ECP_LIGHT */
-#endif /* MBEDTLS_ECP_WITH_MPI_UINT */
+#endif /* 0 && !MBEDTLS_ECP_WITH_MPI_UINT */
