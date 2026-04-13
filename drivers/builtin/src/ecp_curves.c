072d46a1660b2d21cb8eb17d437efdb6bbfa48bd
Manuel Pégourié-Gonnard <manuel.pegourie-gonnard@arm.com>
2026-04-13 12:34:03 +0200

    ecp: fix implementation-defined behaviour
    The comment about optimizing compiler's likely behaviour has been
    checked with GCC and Clang on x86-64, 32-bit Arm and 64-bit Arm.
    
    Signed-off-by: Manuel Pégourié-Gonnard <manuel.pegourie-gonnard@arm.com>

--- drivers/builtin/src/ecp_curves.c
+++ drivers/builtin/src/ecp_curves.c
@@ -3937,7 +3937,16 @@ int mbedtls_ecp_group_load(mbedtls_ecp_group *grp, mbedtls_ecp_group_id id)
 
 static inline int8_t extract_carry(int64_t cur)
 {
-    return (int8_t) (cur >> 32);
+    /*
+     * Extract the signed carry without relying on `cur >> 32` for negative `cur`,
+     * since signed right shift is implementation-defined. Shift as uint64_t, then
+     * sign-extend explicitly.
+     *
+     * Modern optimizing compilers will likely generate the same code
+     * as the less portable (int8_t) (cur >> 32) anyway.
+     */
+    uint32_t hi = (uint32_t) (((uint64_t) cur) >> 32);
+    return (int8_t) ((int64_t) hi - ((int64_t) (hi >> 31) << 32));
 }
 
 #define ADD(j)    cur += A(j)
