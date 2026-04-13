93a126b569206169ebc1f50d6f9ef371459b8470
Gilles Peskine <Gilles.Peskine@arm.com>
2026-04-13 14:19:16 +0200

    Remove test data files that don't exist in 1.1
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- scripts/maintainer/generate_mldsa_tests.py
+++ scripts/maintainer/generate_mldsa_tests.py
@@ -17,10 +17,6 @@ class MLDSATestGenerator(test_data_generation.TestGenerator):
     def __init__(self, settings) -> None:
         self.targets = {
             'test_suite_pqcp_mldsa.dilithium_py': mldsa_test_generator.gen_pqcp_mldsa_all,
-            'test_suite_psa_crypto_mldsa.dilithium_py': \
-            lambda: mldsa_test_generator.DriverGenerator().gen_all(),
-            'test_suite_dispatch_transparent.dilithium_py': \
-            lambda: mldsa_test_generator.DispatchGenerator().gen_all(),
         }
         super().__init__(settings)
 
