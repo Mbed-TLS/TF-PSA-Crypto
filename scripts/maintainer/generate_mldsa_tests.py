c426aae64741656cb9060ac9fd1ac9a9ecacad76
Gilles Peskine <Gilles.Peskine@arm.com>
2026-04-20 15:19:17 +0200

    Add generated multipart tests for ML-DSA driver
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- scripts/maintainer/generate_mldsa_tests.py
+++ scripts/maintainer/generate_mldsa_tests.py
@@ -18,7 +18,7 @@ class MLDSATestGenerator(test_data_generation.TestGenerator):
         self.targets = {
             'test_suite_pqcp_mldsa.dilithium_py': mldsa_test_generator.gen_pqcp_mldsa_all,
             'test_suite_psa_crypto_mldsa.dilithium_py': \
-            lambda: mldsa_test_generator.DriverGenerator().gen_all(),
+            lambda: mldsa_test_generator.DriverGenerator().gen_all(multipart=True),
             'test_suite_dispatch_transparent.dilithium_py': \
             lambda: mldsa_test_generator.DispatchGenerator().gen_all(),
         }
