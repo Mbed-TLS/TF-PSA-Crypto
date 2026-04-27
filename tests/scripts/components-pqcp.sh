17781975aa56229bf31ebc973ad886e3d52b2eab
Ben Taylor <ben.taylor@linaro.org>
2026-04-27 09:12:01 +0100

    Add functional tests for mldsa-native
    Signed-off-by: Ben Taylor <ben.taylor@linaro.org>

---
+++ tests/scripts/components-pqcp.sh
@@ -0,0 +1,26 @@
+# components-pqcp.sh
+#
+# Copyright The Mbed TLS Contributors
+# SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later
+
+# This file contains test components that are executed by all.sh
+
+################################################################
+#### PQCP Testing
+################################################################
+
+support_test_pqcp_mldsa_native_upstream_all () {
+    python3 -c 'import sys; exit(1 if sys.version_info < (3, 9) else 0)'
+}
+
+# This condition deliberately runs when all.sh sources this file. It hides the
+# component from both `--list-all-components` and explicit component requests on
+# runners whose Python is too old for mldsa-native's generated-test checks.
+if support_test_pqcp_mldsa_native_upstream_all; then
+    component_test_pqcp_mldsa_native_upstream_all () {
+        msg "test: mldsa-native upstream all --opt ALL --examples"
+
+        cd drivers/pqcp/mldsa-native
+        ./scripts/tests all --opt ALL --examples
+    }
+fi
