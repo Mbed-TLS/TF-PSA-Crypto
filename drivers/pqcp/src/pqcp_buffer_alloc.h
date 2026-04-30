5bc94881c45ae9dbda7d298fa3eb1093f871c77a
Bence Szépkúti <bence.szepkuti@arm.com>
2026-04-30 09:35:31 +0200

    Add threading support to the mldsa allocator
    Signed-off-by: Bence Szépkúti <bence.szepkuti@arm.com>

--- drivers/pqcp/src/pqcp_buffer_alloc.h
+++ drivers/pqcp/src/pqcp_buffer_alloc.h
@@ -11,6 +11,10 @@
 #if defined(TF_PSA_CRYPTO_PQCP_MLDSA_ENABLED) && defined(TF_PSA_CRYPTO_PQCP_BUFFER_ALLOC)
 #include <psa/crypto_values.h>
 
+#if defined(MBEDTLS_THREADING_C)
+#include "threading_internal.h"
+#endif
+
 // Sufficient for signing with TF_PSA_CRYPTO_PQCP_MLDSA_87_ENABLED
 #define TF_PSA_CRYPTO_PQCP_ALLOC_BUFFER_SIZE 123200
 
@@ -20,26 +24,49 @@ struct tf_psa_crypto_pqcp_context {
     int alloc_offset; // Negative values indicate allocator errors
 };
 
+#if defined(MBEDTLS_THREADING_C)
+#define TF_PSA_CRYPTO_PQCP_ALLOC_LOCK() \
+    mbedtls_mutex_lock(&mbedtls_threading_pqcp_buffer_alloc_mutex)
+#else
+#define TF_PSA_CRYPTO_PQCP_ALLOC_LOCK() 0
+#endif
+
 #define TF_PSA_CRYPTO_PQCP_CUSTOM_ALLOC(v, T, N, context) \
     T *(v) = NULL; \
     do { \
         /* Verify that the allocation would fit in the buffer by itself, avoiding overflows \
            This should be optimized away at compile-time */ \
         if ((N) > 0 && (N) <= TF_PSA_CRYPTO_PQCP_ALLOC_BUFFER_SIZE / sizeof(T)) { \
+            if ((context).alloc_offset == 0) { \
+                (context).alloc_offset = TF_PSA_CRYPTO_PQCP_ALLOC_LOCK(); \
+            } \
             if ((context).alloc_offset >= 0) { \
                 if ((size_t) (context).alloc_offset <= \
                     TF_PSA_CRYPTO_PQCP_ALLOC_BUFFER_SIZE - MLD_ALIGN_UP(sizeof(T) * (N))) { \
                     (v) = (T *) (tf_psa_crypto_pqcp_alloc_buffer + (context).alloc_offset); \
                     (context).alloc_offset += MLD_ALIGN_UP(sizeof(T) * (N)); \
                 } else { \
-                    /* Fail all further allocations in this function -> goto cleanup */ \
+                    /* Fail all further allocations in this function -> goto cleanup \
+                     * If we ended up here, that implies (alloc_offset != 0) \
+                     * thus we don't need to call mutex_unlock() */ \
                     (context).alloc_offset = PSA_ERROR_INSUFFICIENT_MEMORY; \
                 } \
             } \
         } \
     } while (0)
 
+#if defined(MBEDTLS_THREADING_C)
+// mldsa-native only calls this macro if v != NULL
+#define TF_PSA_CRYPTO_PQCP_CUSTOM_FREE(v, T, N, context) \
+    do { \
+        /* Unlock only after freeing the last allocation */ \
+        if ((uint8_t *) (v) == tf_psa_crypto_pqcp_alloc_buffer) { \
+            mbedtls_mutex_unlock(&mbedtls_threading_pqcp_buffer_alloc_mutex); \
+        } \
+    } while (0)
+#else
 #define TF_PSA_CRYPTO_PQCP_CUSTOM_FREE(v, T, N, context)
+#endif
 
 #endif /* TF_PSA_CRYPTO_PQCP_MLDSA_ENABLED && TF_PSA_CRYPTO_PQCP_BUFFER_ALLOC */
 
