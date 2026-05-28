7d1f5a972c8d8b8687dd4fd47fb69450e04dfd55
Manuel Pégourié-Gonnard <manuel.pegourie-gonnard@arm.com>
2026-05-28 10:57:13 +0200

    everest: better CT checking of all-0 output
    Signed-off-by: Manuel Pégourié-Gonnard <manuel.pegourie-gonnard@arm.com>

--- drivers/builtin/src/psa_crypto_ecp.c
+++ drivers/builtin/src/psa_crypto_ecp.c
@@ -26,6 +26,7 @@
 
 #if defined(MBEDTLS_ECDH_VARIANT_EVEREST_ENABLED)
 #include "tf-psa-crypto/private/everest/x25519.h"
+#include "mbedtls/constant_time.h"
 #endif
 
 #if defined(MBEDTLS_PSA_BUILTIN_KEY_TYPE_ECC_KEY_PAIR_BASIC) || \
@@ -565,11 +566,8 @@ static psa_status_t ecdh_everest_shared_secret(
      * 7748, but ecp.c does it (by checking if the input is a low-order point,
      * see ecp_check_bad_points_mx()), and protocols that require contributory
      * behaviour need it. Also, it will never happen in normal ECDH usage. */
-    uint8_t s = 0;
-    for (size_t i = 0; i < *shared_secret_length; i++) {
-        s |= shared_secret[i];
-    }
-    if (s == 0) {
+    uint8_t zero[MBEDTLS_X25519_KEY_SIZE_BYTES] = { 0 };
+    if (mbedtls_ct_memcmp(zero, shared_secret, *shared_secret_length) == 0) {
         return PSA_ERROR_INVALID_ARGUMENT;
     }
 
