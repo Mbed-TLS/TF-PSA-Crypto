c677c1b63d6f1d8dd691606ad56b063fd4b543ac
Ben Taylor <ben.taylor@linaro.org>
2026-07-02 11:54:20 +0100

    Add the alloc test to the mldsa test suite
    Signed-off-by: Ben Taylor <ben.taylor@linaro.org>

--- tests/scripts/components-pqcp.sh
+++ tests/scripts/components-pqcp.sh
@@ -16,5 +16,5 @@ support_test_pqcp_mldsa_native_upstream_all () {
 component_test_pqcp_mldsa_native_upstream_all () {
     msg "test: mldsa-native upstream all --opt ALL --examples --stack"
      cd drivers/pqcp/mldsa-native
-    ./scripts/tests all --opt ALL --examples --stack
+    ./scripts/tests all --opt ALL --examples --stack --alloc
 }
