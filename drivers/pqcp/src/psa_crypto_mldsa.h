0e425fbdbe37d0b1bf8431d4b9a495eb0cb361de
Gilles Peskine <Gilles.Peskine@arm.com>
2026-08-14 19:58:27 +0200

    ML-DSA: driver entry point to generate a key in the standard format
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- drivers/pqcp/src/psa_crypto_mldsa.h
+++ drivers/pqcp/src/psa_crypto_mldsa.h
@@ -76,6 +76,36 @@ psa_status_t tf_psa_crypto_mldsa_export_public_key(
     const uint8_t *key_buffer, size_t key_buffer_size,
     uint8_t *data, size_t data_size, size_t *data_length);
 
+/** Generate a random ML-DSA key pair in the PSA standard format.
+ *
+ * \note You can use tf_psa_crypto_mldsa_generate_expanded_key_pair()
+ *       to directly obtain the private key in joined format (seed
+ *       concatenated with the standard epxanded format) and the public key.
+ *
+ * \param[in] attributes        The key attributes.
+ *                              The key type must be
+ *                              #PSA_KEY_TYPE_ML_DSA_KEY_PAIR,
+ *                              and the bit-size must be one of the supported
+ *                              parameter sets (currently: only 67).
+ * \param[out] seed             The private key in the PSA standard format,
+ *                              i.e. the 32-byte seed:
+ * \param seed_size             The size of \p seed, in bytes.
+ * \param[out] seed_length      On success, the length of the data written
+ *                              to \p seed.
+ *
+ * \retval #PSA_SUCCESS
+ *         Success.
+ * \retval #PSA_ERROR_NOT_SUPPORTED
+ *         The key type or size registered in \p attributes is not supported.
+ * \retval #PSA_ERROR_INSUFFICIENT_MEMORY
+ *         Random generator failure.
+ * \retval #PSA_ERROR_BUFFER_TOO_SMALL
+ *         \p seed_size is too small.
+ */
+psa_status_t tf_psa_crypto_mldsa_generate_key(
+    const psa_key_attributes_t *attributes,
+    uint8_t *seed, size_t seed_size, size_t *seed_length);
+
 /** Sign a message using pure-ML-DSA (without pre-hashing).
  *
  * \param[in] attributes        The key attributes.
