6fbf0eb587939e1eb58ea46a8c92be4b76288bb6
Ronald Cron <ronald.cron@arm.com>
2026-08-25 10:05:19 +0200

    Create source files for the SM3 implementation
    Signed-off-by: Ronald Cron <ronald.cron@arm.com>

---
+++ drivers/builtin/src/sm3.c
@@ -0,0 +1,13 @@
+/**
+ * \file sm3.c
+ *
+ * \brief Implementation of SM3.
+ */
+/*
+ *  Copyright The Mbed TLS Contributors
+ *  SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later
+ */
+
+#include "tf_psa_crypto_common.h"
+
+#include "sm3_internal.h"
---
+++ drivers/builtin/src/sm3_internal.h
@@ -0,0 +1,18 @@
+/**
+ * \file sm3_internal.h
+ *
+ * \brief This file contains internal function definitions for SM3.
+ */
+/*
+ *  Copyright The Mbed TLS Contributors
+ *  SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later
+ */
+
+#ifndef TF_PSA_CRYPTO_SM3_INTERNAL_H
+#define TF_PSA_CRYPTO_SM3_INTERNAL_H
+#include "mbedtls/private_access.h"
+
+#include "tf-psa-crypto/build_info.h"
+
+
+#endif /* TF_PSA_CRYPTO_SM3_INTERNAL_H */
