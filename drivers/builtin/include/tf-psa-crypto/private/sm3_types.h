a6434c658f2fdcab5a6457328a87e65ea23b437d
Ronald Cron <ronald.cron@arm.com>
2026-08-25 10:05:19 +0200

    Create source files for the SM3 implementation
    Signed-off-by: Ronald Cron <ronald.cron@arm.com>

---
+++ drivers/builtin/include/tf-psa-crypto/private/sm3_types.h
@@ -0,0 +1,29 @@
+/**
+ * \file sm3_types.h
+ *
+ * \brief This file contains context types for SM3.
+ */
+/*
+ *  Copyright The Mbed TLS Contributors
+ *  SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later
+ */
+
+#ifndef TF_PSA_CRYPTO_PRIVATE_SM3_TYPES_H
+#define TF_PSA_CRYPTO_PRIVATE_SM3_TYPES_H
+#include "mbedtls/private_access.h"
+
+#include "tf-psa-crypto/build_info.h"
+
+#include <stddef.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TF_PSA_CRYPTO_PRIVATE_SM3_TYPES_H */
